ac3703c47996ce9aff293e563428be10acf6daeb
Patrick Alves <cpatrickalves@gmail.com>
2023-10-06 15:05:53 -0300

    Cria modelo de dados

---
+++ inscricoes/model.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Field, SQLModel, create_engine
+
+
+def get_sql_engine():
+    engine = create_engine("sqlite:///data/inscricoes.db")
+    SQLModel.metadata.create_all(engine)
+
+    return engine
+
+
+class Participante(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
+    idade: int
+    sexo: str
+    isento: bool
+    tipo_inscricao: str
+    valor_inscricao: float
+    inscricao_id: int = Field(foreign_key="inscricao.id")
+
+
+class Conferencia(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
+    data: str
+    local: str
+
+
+class Inscricao(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    conferencia_id: int = Field(foreign_key="conferencia.id")
+    cidade: str
+    estado: str
+    responsavel: str
+    data: datetime = Field(default=datetime.now())
+    pg_total: Optional[float] = Field(default=0.0)
+    pg_dinheiro: Optional[float] = Field(default=0.0)
+    pg_cheque: Optional[float] = Field(default=0.0)
+    pg_cartao: Optional[float] = Field(default=0.0)
+    pg_transferencia: Optional[float] = Field(default=0.0)
+    pg_deposito: Optional[float] = Field(default=0.0)
+    pg_pendencias: Optional[float] = Field(default=0.0)
+    observacoes: Optional[str] = Field(default="")
